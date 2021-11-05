31eb87f6a47e773a98f4890a5f99f554f739bfd7
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-05 11:52:57 +0530

    migrate posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_060148) do
+ActiveRecord::Schema.define(version: 2021_11_05_062152) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "published"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
