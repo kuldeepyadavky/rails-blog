cbdcdc656482196dd6dfea8d231ba6f1cc71ab56
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-05 17:32:55 +0530

    playing with scopes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,12 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.includes(:category).all
+    @posts_published = Post.published
+    @posts_unpublished = Post.unpublished
+    @top_5 = Post.limit_5
+    @latest_posts = Post.order_by_latest_first
+    @all_combined = Post.published.order_by_latest_first.limit_5
+
   end
 
   # GET /posts/1 or /posts/1.json
@@ -22,6 +28,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+
 
     respond_to do |format|
       if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
     has_many  :taggings #has many taggings #join table
     has_many :tags, through: :taggings #tags through taggings
 
+    scope :published, lambda {where(published: true)}
+    scope :unpublished, -> {where(published: false)}
+    scope :limit_5, -> {limit(5)}
+
+    scope :order_by_latest_first, -> {order(created_at: :desc)}
+
     def all_tags=(names)
         if names.blank?
             return
