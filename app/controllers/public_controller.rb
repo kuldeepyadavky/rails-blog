3c723d1f6d71f13884c941e621d9e5c91f886e7a
Kuldeep Yadav <kuldeepyadav@LA-183.local>
2021-11-06 05:01:13 +0530

    admin controller, search by tags;

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,4 +3,12 @@ class PublicController < ApplicationController
     # @posts = Post.all
     @posts = Post.order(created_at: :desc)
   end
+
+  def tags 
+    tag = params[:tag]
+    # @posts = Post.tags.where(name: tag)
+    # render json: @posts
+    @posts = Tag.find_by(:name => tag).posts
+    render json: @posts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+  get "public/tags/:tag", to: "public#tags", :as => :tag_search
+
   resources :tags
   resources :categories
   resources :posts
   devise_for :users
   get 'public/index' #controller public and action index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    #resources :post
+    get '/posts', to: 'posts#index'
+  end
 
   root to: "public#index"  #adding a redirect route
 end
